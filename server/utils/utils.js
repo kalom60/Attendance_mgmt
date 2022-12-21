d4ceece4e058e1911ae71de14094738f73665940
kalom60 <kaleabtesfaye00@gmail.com>
2022-12-21 14:39:08 +0300

    add image removing utility

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt'
+import fs from 'fs'
 
 class Utils {
     static async hashPassword(pass) {
@@ -7,6 +8,17 @@ class Utils {
         } catch (err) {
             console.log(err)
         }
+    }
+
+    static removeImage(images) {
+        const img1 = images.teacher_image.split('/')[3]
+        const img2 = img1.slice(0, 5) + '/uploads/' + img1.slice(6)
+        fs.unlinkSync(img1, (err) => {
+            if (err) console.log(err)
+        })
+        fs.unlinkSync(img2, (err) => {
+            if (err) console.log(err)
+        })
     }
 }
 
